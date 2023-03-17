b7d8d0b4283484ab85cf6b204ca4843d1bd2f137
Houzifa Habbo <h-hebbo@hotmail.com>
2023-03-17 19:13:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const employee = {
-    name:"Sam",
-    streetAddress:"paris"
+    name: "Sam",
+    streetAddress: "paris"
 }
-function updateEmployeeWithKeyAndValue(employee,key,value){
-    const newObj = {...employee};
-    newObj [key] = value;
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee };
+    newObj[key] = value;
     return newObj;
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee ,key ,value){
-    employee [key] = value;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
     return employee;
 }
-function deleteFromEmployeeByKey(employee , key){
-    const newObj = {...employee};
-    delete newObj [key];
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = { ...employee };
+    delete newObj[key];
     return newObj;
 }
-function destructivelyDeleteFromEmployeeByKey(employee , key){
-    delete employee [key];
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
     return employee;
 }
